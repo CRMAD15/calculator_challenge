fb6ee28eb9e52922087dfe8f3ed6482e5aaa5c29
Cristian Calderon <cristianfab90@gmail.com>
2022-09-09 16:09:19 +0200

    logig part

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
+import React, { useRef, useState, useEffect } from 'react';
 import './App.css';
 import Numbers from './components/numbers/numbers';
 
 function App() {
+  const [result, setResult] = useState('');
+
+  const focusPoint = useRef()
+  useEffect(() => {
+    focusPoint.current.focus()
+  }, []);
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString())
+
+    } catch (error) {
+      setResult('Error')
+    }
+  }
+  const handleChange = e => {
+    e.preventDefault();
+    setResult(e.target.value.replace(/(\d{1,4}([.\-/])\d{1,2}([.\-/])\d{1,4})/g,))
+
+  };
+
 
   return (
     <div className='container'>
       <header >
         <div>
+          <h1>calc</h1>
+        </div>
+        <div>
           <form>
-            <input className='input_text' placeholder='hola'></input>
+            <input
+              className='input_text'
+              placeholder='0'
+              ref={focusPoint}
+              value={result}
+              onChange={handleChange}
+            />
           </form>
         </div>
         <div className='keyboard'>
-          <Numbers />
+          <Numbers
+            result={result}
+            setResult={setResult}
+            calculate={calculate}
+          />
         </div>
       </header>
     </div>
--- src/components/numbers/numbers.jsx
+++ src/components/numbers/numbers.jsx
@@ -1,38 +1,51 @@
 import React from 'react';
 import './numbers.css'
-const Numbers = () => {
+const Numbers = ({ result, setResult, calculate }) => {
+    const handleClick = e => {
 
+        result === 'Error' ? setResult(e.target.name) : setResult(result.concat(e.target.name.replace(/(\d{1,4}([.\-/])\d{1,2}([.\-/])\d{1,4})/g)));
+    };
+
+    const resetResult = () => {
+        setResult('')
+    }
+
+    const backDelete = () => {
+        result === 'Error' ? setResult('') : setResult(result.slice(0, -1))
+    }
     return (
         <div>
             <div className='numbers'>
-                <button className='btn white'>7</button>
-                <button className='btn white'>8</button>
-                <button className='btn white'>9</button>
-                <button className='btn blue'>DEL</button>
+                <button name='7' className='btn white' onClick={handleClick}>7</button>
+                <button name='8' className='btn white' onClick={handleClick}>8</button>
+                <button name='9' className='btn white' onClick={handleClick}>9</button>
+                <button className='btn blue' onClick={backDelete}>DEL</button>
             </div>
             <div className='numbers'>
-                <button className='btn white'>4</button>
-                <button className='btn white'>5</button>
-                <button className='btn white'>6</button>
-                <button className='btn white'>+</button>
+                <button name='4' className='btn white' onClick={handleClick}>4</button>
+                <button name='5' className='btn white' onClick={handleClick}>5</button>
+                <button name='6' className='btn white' onClick={handleClick}>6</button>
+                <button name='+' className='btn white' onClick={handleClick}>+</button>
             </div>
             <div className='numbers'>
-                <button className='btn white'>1</button>
-                <button className='btn white'>2</button>
-                <button className='btn white'>3</button>
-                <button className='btn white'>-</button>
+                <button name='1' className='btn white' onClick={handleClick}>1</button>
+                <button name='2' className='btn white' onClick={handleClick}>2</button>
+                <button name='3' className='btn white' onClick={handleClick}>3</button>
+                <button name='-' className='btn white' onClick={handleClick}>-</button>
             </div>
             <div className='numbers'>
-                <button className='btn white'>.</button>
-                <button className='btn white'>0</button>
-                <button className='btn white'>/</button>
-                <button className='btn white'>X</button>
+                <button name='.' className='btn white' onClick={handleClick}>.</button>
+                <button name='0' className='btn white' onClick={handleClick}>0</button>
+                <button name='/' className='btn white' onClick={handleClick}>/</button>
+                <button name='*' className='btn white' onClick={handleClick}>X</button>
             </div>
             <div className='numbers'>
-                <button className='btn blue width'>RESET</button>
-                <button className='btn red width'>=</button>
+                <button className='btn blue width' onClick={resetResult}>RESET</button>
+                <button className='btn red width' onClick={calculate}>=</button>
             </div>
         </div>
+
+
 
     );
 }
